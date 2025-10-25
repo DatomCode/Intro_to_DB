7600bef23cace1be0719e6a706a19404d1ff407a
Datom-code <enochgbadebo007@gmail.com>
2025-10-25 13:36:48 +0100

    third commit

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,5 @@
 USE alx_book_store;
--- Insert a new record into the customer table
+
+
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Ama Serwaa', 'aserwaa@sandtech.com', '45 Peace Street, Accra');
-
-
+VALUES (3, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
