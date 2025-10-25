8581bbd357309937aaff45d33a2da93dfc2c917d
Datom-code <enochgbadebo007@gmail.com>
2025-10-25 13:35:17 +0100

    second commit

--- task_6.sql
+++ task_6.sql
@@ -1,18 +1,6 @@
 USE alx_book_store;
-Data insertion
+-- Insert a new record into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, 'Ama Serwaa', 'aserwaa@sandtech.com', '45 Peace Street, Accra');
 
-    - `customer_id` = `2`
-    - `customer_name` = `Blessing Malik`
-    - `email` = `bmalik@sandtech.com`
-    - `address` = `124 Happiness  Ave.`
-    - `customer_id` = `3`
-    - `customer_name` = `Obed Ehoneah`
-    - `email` = `eobed@sandtech.com`
-    - `address` = `125 Happiness  Ave.`
-    - `customer_id` = `4`
-    - `customer_name` = `Nehemial Kamolu`
-    - `email` = `nkamolu@sandtech.com`
-    - `address` = `126 Happiness  Ave.`
-The database name will be passed as an argument of the mysql command
 
-The name of the file should be task_6.sql
